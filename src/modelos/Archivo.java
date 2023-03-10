c7aed69a6e8e8c45606eaed3e73117c5f00d7862
gabrielZelaya0821 <gabriel.zelaya2005@gmail.com>
2023-03-10 13:52:42 -0600

    Modelos terminados y bg principal terminado

---
+++ src/modelos/Archivo.java
@@ -0,0 +1,55 @@
+package modelos;
+
+/**
+ *
+ * @author gabri
+ */
+public class Archivo {
+    private String nombre;
+    private String link;
+    private String extension;
+    private double tamaño;
+
+    public Archivo() {
+    }
+
+    public Archivo(String nombre, String link, String extension, double tamaño) {
+        this.nombre = nombre;
+        this.link = link;
+        this.extension = extension;
+        this.tamaño = tamaño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public double getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(double tamaño) {
+        this.tamaño = tamaño;
+    }
+    
+}
---
+++ src/modelos/Carpeta.java
@@ -0,0 +1,54 @@
+package modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gabri
+ */
+public class Carpeta {
+    private String nombre;
+    private String link;
+    private ArrayList<Archivo> archivos;
+    private ArrayList<Carpeta> carpetas;
+
+    public Carpeta(String nombre, String link, ArrayList<Archivo> archivos, ArrayList<Carpeta> carpetas) {
+        this.nombre = nombre;
+        this.link = link;
+        this.archivos = archivos;
+        this.carpetas = carpetas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ArrayList<Archivo> getArchivos() {
+        return archivos;
+    }
+
+    public void setArchivos(ArrayList<Archivo> archivos) {
+        this.archivos = archivos;
+    }
+
+    public ArrayList<Carpeta> getCarpetas() {
+        return carpetas;
+    }
+
+    public void setCarpetas(ArrayList<Carpeta> carpetas) {
+        this.carpetas = carpetas;
+    }
+    
+}
